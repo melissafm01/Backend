680103f9b7fda41b79102ff21239da4098649824
Melissaf M <melissam200604@gmail.com>
2025-04-29 23:04:20 -0500

    backend modularizado

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import taksRoutes from "./routes/tasks.routes.js";
+import { FRONTEND_URL } from "./config.js";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api", taksRoutes);
+
+export default app;
