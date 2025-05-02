c6a9ab9f2ae8646322a1e9ee8858c601056a1ae0
Melissaf M <melissam200604@gmail.com>
2025-05-02 11:06:50 -0500

    config produccion

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =  process.env.MONGODB_URI || "mongodb+srv://melissam200604:panas2025@cluster0.abndtzx.mongodb.net/panascOOp?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://fronted-sooty-seven.vercel.app";
