bca77ba975358f765c19a7f523296a0781c40140
Melissaf M <melissam200604@gmail.com>
2025-05-02 10:17:27 -0500

    configuracion de url

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 export const MONGODB_URI =  process.env.MONGODB_URI || "mongodb+srv://melissam200604:panas2025@cluster0.abndtzx.mongodb.net/panascOOp?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://fronted-eight-omega.vercel.app";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://fronted-sooty-seven.vercel.app";
