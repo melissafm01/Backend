420c7e585d755f4761f6884db109012675cc7301
Melissaf M <melissam200604@gmail.com>
2025-05-04 13:55:05 -0500

    configuracion de variables

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ async function main() {
   try {
     await connectDB();
     app.listen(PORT);
-    console.log(`Listening on port https://backend-lzl9.onrender.com`);
+    console.log(`Listening on port http://localhost:${PORT}`);
     console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {
     console.error(error);
