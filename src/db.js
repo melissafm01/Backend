1a868fb3d7381cf3f9e3bcfb0ce5715b2912975b
Melissaf M <melissam200604@gmail.com>
2025-05-14 19:06:36 -0500

    configuracion en config

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { MONGODB_URI } from "./config.js";
+import {MONGODB_URL } from "./config.js";
 
 export const connectDB = async () => {  
   try {
-    await mongoose.connect(MONGODB_URI);   
+    await mongoose.connect(MONGODB_URL);   
     console.log("MongoDB is connected");
   } catch (error) {
     console.error(error);
