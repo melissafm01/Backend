148fb87661d18b61ea3c6809ef3bbfe923579dae
Melissaf M <melissam200604@gmail.com>
2025-06-20 18:40:31 -0500

    .

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,9 +8,12 @@ dotenv.config();
 let serviceAccount;
 
 if (process.env.FIREBASE_CONFIG) {
-  // ✅ En producción (Render): lee desde variable de entorno
-  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-} else {
+  // En producción (Render): lee desde variable de entorno y repara los \n
+  const raw = JSON.parse(process.env.FIREBASE_CONFIG);
+  raw.private_key = raw.private_key.replace(/\\n/g, '\n'); // <- ESTA LÍNEA ES LA CLAVE
+  serviceAccount = raw;
+
+}else {
   // ✅ En local: lee desde archivo
   const serviceAccountPath = path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
   serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf-8'));
